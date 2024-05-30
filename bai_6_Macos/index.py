926ec34eec0f0d1f5c2780e2d53886b1911a83eb
Cris Vu <vva1994@gmail.com>
2024-05-30 19:05:23 +0700

    add

--- bai_6_Macos/index.py
+++ bai_6_Macos/index.py
@@ -6,22 +6,28 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
 from selenium.webdriver.chrome.service import Service
 
+#TODO Init chrome
 service = Service(excutable_path="chromedriver")
 driver = webdriver.Chrome(service=service)
-
+#TODO
 driver.get("https://google.com")  # truy cap link
 WebDriverWait(driver, 5).until(
     EC.presence_of_element_located((By.CLASS_NAME, "gLFyf"))
 )
 
 input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
-# input_element.clear()
-input_element.send_keys("tech with me" + Keys.ENTER)
+input_element.clear()
+input_element.send_keys("tech with Tim" + Keys.ENTER)
 
-# WebDriverWait(driver)
+#todo Fine link text
+WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, "Tech With Tim"))
+)
+link = driver.find_element(By.PARTIAL_LINK_TEXT, "Tech With Tim")
+link.click()
+
+
 time.sleep(10)
-
 driver.quit()
